88ce4b2a0c615fc2d6f0325fe4cfb33630b7c6f9
hikalium <hikalium@hikalium.com>
2022-07-02 00:05:56 +0900

    update

--- hikalium/build.sh
+++
@@ -1,5 +0,0 @@
-export BUILD_DIR=`readlink -f .`/build/HEAD
-echo ${BUILD_DIR}
-mkdir -p ${BUILD_DIR}
-make -C .. O=${BUILD_DIR} x86_64_defconfig
-make -C .. O=${BUILD_DIR} -j `nproc` vmlinux
--- hikalium/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -xe
-VMLINUX=`readlink -f build/HEAD/vmlinux`
-cd ../../crosvm
-cargo run -- run ${VMLINUX}
-
